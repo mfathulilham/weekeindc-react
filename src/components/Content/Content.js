42aa1d2ecd47b0afd9189d7245fc36d5dc1b7b7f
mfathulilham <mfathulilhamm@gmail.com>
2022-05-09 08:33:47 +0800

    Add Animate On Scroll Library

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -33,10 +33,10 @@ function Content() {
 
   return (
     <div className='content'>
-      <img src={images.path_pink4} alt='path_pink' className='path-pink4' />
-      <img src={images.astronout} alt='astronout' className='astronout'/>
+      <img data-aos="fade-up-right" src={images.path_pink4} alt='path_pink' className='path-pink4' />
+      <img data-aos="fade-up-right" src={images.astronout} alt='astronout' className='astronout'/>
       
-      <div className='testimonial'>
+      <div data-aos="fade-right" className='testimonial'>
         <h1 className='title'>Testimonial</h1>
         <div className='card-wrapper'>
           <SimpleSlider>
@@ -50,18 +50,19 @@ function Content() {
         </div>
       </div>
 
-      <div className='pov'>
+      <div data-aos="fade-up-left" className='pov'>
         <h1>POV</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ullamco laboris nisi ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
       </div>
       
-      <div className='resource'>
+      <div data-aos="fade-up-right" className='resource'>
         <h1>Resource</h1>
         <p>These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best</p>
       </div>
       
-      <div className='help'>
+      <div data-aos="fade-left" className='help'>
           <h1>Help &amp; Tips</h1>
+          <img data-aos="fade-up-right" src={images.path_pink3} alt='path_pink' className='path-pink3' />
           
           <div className='card-wrapper'>
               { helps?.map((help) => (
@@ -75,7 +76,7 @@ function Content() {
           </div>
       </div>
 
-      <div className='set'>
+      <div data-aos="fade-down-right" className='set'>
           <h1>You're all set.</h1>
           <p>The wise man therefore always holds in these matters to this principle of selection.</p>
       </div>
--- src/components/Quotes/Quotes.js
+++ src/components/Quotes/Quotes.js
@@ -5,10 +5,10 @@ import './Quotes.style.scss';
 function Quotes() {
   return (
     <div className='quotes'>
-        <img src={images.path_pink2} alt='path_pink' className='path-pink' />
-        <img src={images.astronout} alt='astronout' className='img-astronout' />
+        <img data-aos="fade-down-left" src={images.path_pink2} alt='path_pink' className='path-pink' />
+        <img data-aos="fade-down-left" src={images.astronout} alt='astronout' className='img-astronout' />
         <img src={images.circle_blue} alt='circle_blue' className='circle-blue' />
-        <div className='quotes-content'>
+        <div data-aos="fade-right" className='quotes-content'>
             <p>
                 <span>Definition: </span>
                  a practice or exercise to test or improve one's fitness for athletic competition, ability, or performance to exhaust (something, such as a mine) by working to devise, arrange, or achieve by resolving difficulties. Merriam-Webster.com Dictionary.
